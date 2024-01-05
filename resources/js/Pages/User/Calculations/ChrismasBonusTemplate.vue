e7cdfac3b6662e7535c83924990d2745b389b0a1
Angel Vazquez <angelvazquez470@gmail.com>
2024-01-05 16:15:59 -0600

    calculos de usuario. Aguinaldo y finiquito

--- resources/js/Pages/User/Calculations/ChrismasBonusTemplate.vue
+++ resources/js/Pages/User/Calculations/ChrismasBonusTemplate.vue
@@ -1,24 +1,31 @@
 <template>
-  <div class="grid grid-cols-2 gap-1">
-    <div class="rounded-md border-2 border-gray-400 px-3 py-2 leading-none text-xs">
+  <div class="">
+    <div class="rounded-md border-2 border-gray-400 px-7 py-2 leading-none text-xs">
       <div class="globe-title">
-        <h1>Cálculo de Aguinaldo año {{ today.getFullYear() }}</h1>
+        <h1>Aguinaldo {{ today.getFullYear() }}</h1>
       </div>
       <p style="font-size: 8px">
         Av. Manuel Ávila Camacho 1950, Interior de plaza patria, Isla comercial en frente
         de negocio comercial conocido como "ALDO CONTI", en el área común, Col.
         Jacarandas, Zapopan Jalisco.
       </p>
-      <div class="mb-2 bg-sky-200 px-1 py-px rounded-sm">
-        <i class="fa-solid fa-user mr-1"></i> {{ user.name }}
+      <div class="mb-2 bg-[#EDEDED] rounded-sm py-2 px-3 my-2 font-bold">
+        <i class="fa-regular fa-user mr-1"></i> {{ user.name }}
       </div>
-      <div class="flex flex-col space-y-2 mb-4">
-        <p><strong> Fecha de Ingreso: </strong>{{ user.created_at.split('T')[0] }}</p>
-        <p><strong> Fin de Ejercicio: </strong>{{ '31-Diciembre -' + today.getFullYear() }}</p>
+      <div class="flex flex-col space-y-3 my-4">
+        <p><strong> Fecha de Ingreso: </strong>{{ formattedCurrentDate(user.created_at) }}</p>
+        <p><strong> Fin de Ejercicio: </strong>{{ formattedDate  }}</p>
         <p><strong> Salario diario integrado: </strong>${{ user.employee_properties.base_salary  }}</p>
-        <p><strong> Aguinaldo correspondiente: </strong>${{ chrismas_bonus  }}</p>
-        
+        <p><strong> Aguinaldo correspondiente </strong>${{ chrismas_bonus  }}</p>
+
       </div>
+
+
+      <div class="flex justify-end mt-12 mb-3">
+        <span class="border-t-2 px-20 border-gray-400 text-gray-700 font-bold py-2">Total: ${{ chrismas_bonus }}</span>
+      </div>
+
+
       <small style="font-size: 7px">
         RECIBI DE LA EMPRESA "PURO CHURRO" LA CANTIDAD SEÑALADA MISMA QUE CUBRE LAS
         PERCEPCIONES QUE ME CORRESPONDEN EN EL PERIODO INDICADO, NO EXISTIENDO NINGUN
@@ -28,8 +35,8 @@
         CREDITO.
       </small>
       <div class="flex justify-between mt-4">
-        <span class="border-t-2 px-20 border-gray-400 text-gray-500">Fecha</span>
-        <span class="border-t-2 px-20 border-gray-400 text-gray-500">Firma</span>
+        <span class="border-t-2 px-14 border-gray-400 text-gray-500">Fecha</span>
+        <span class="border-t-2 px-14 border-gray-400 text-gray-500">Firma</span>
       </div>
     </div>
   </div>
@@ -44,6 +51,19 @@ export default {
   props: {
     user: Object,
     chrismas_bonus: Number,
+  },
+  computed: {
+    formattedDate() {
+      const options = { day: '2-digit', month: 'long', year: 'numeric' };
+      return this.today.toLocaleDateString('es-ES', options);
+    },
+     formattedCurrentDate() {
+      return datetime => {
+        const date = new Date(datetime);
+        const options = { day: '2-digit', month: 'long', year: 'numeric' };
+        return date.toLocaleDateString('es-ES', options);
+      };
+    },
   },
 };
 </script>

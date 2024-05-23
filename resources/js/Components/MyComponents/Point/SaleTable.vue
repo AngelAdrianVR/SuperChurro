febb8bcdfdc9a08218bf8c04112094967f16bfc2
Miguel Vázquez <miguelvz26.mv@gmail.com>
2024-05-22 20:26:20 -0600

    arreglo de estilos

--- resources/js/Components/MyComponents/Point/SaleTable.vue
+++ resources/js/Components/MyComponents/Point/SaleTable.vue
@@ -18,14 +18,14 @@
         <div :class="editMode !== null ? 'w-[35%]' : 'w-[15%]'" class="text-base flex items-center">
           <template v-if="editMode !== index">
             <span>${{ getPrice(sale.product) }}</span>
-            <button @click.stop="startEditing(sale, index)"
+            <!-- <button @click.stop="startEditing(sale, index)"
               class="flex items-center justify-center text-primary bg-gray-200 size-5 rounded-full ml-2 mr-1">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="size-[14px]">
                 <path stroke-linecap="round" stroke-linejoin="round"
                   d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
               </svg>
-            </button>
+            </button> -->
           </template>
           <template v-else-if="editMode == index">
             <div class="flex items-center space-x-2">
@@ -76,14 +76,14 @@
           <template v-if="editMode !== index">
             ${{ getPrice(sale.product) }}
             <!-- Condicional en el boton depende de la configuracion seleccionada para no poder editar precio -->
-            <button @click.stop="startEditing(sale, index)"
+            <!-- <button @click.stop="startEditing(sale, index)"
               class="flex items-center justify-center text-primary bg-gray-200 size-4 rounded-full ml-2 mr-1">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                 stroke="currentColor" class="size-3">
                 <path stroke-linecap="round" stroke-linejoin="round"
                   d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125" />
               </svg>
-            </button>
+            </button> -->
           </template>
           <template v-else-if="editMode == index">
             <div class="flex items-center space-x-2">
--- resources/js/Components/MyComponents/Sale/SaleTable.vue
+++ resources/js/Components/MyComponents/Sale/SaleTable.vue
@@ -1,58 +1,55 @@
 <template>
   <table class="w-full mx-auto text-sm mt-5">
-        <thead>
-            <tr class="text-center">
-                <th class="font-bold pb-1 pl-2 text-left border-b border-primary">
-                Producto
-                </th>
-                <th class="font-bold pb-1 text-left border-b border-primary">
-                Cantidad
-                </th>
-                <th class="font-bold pb-1 text-left border-b border-primary">
-                Total
-                </th>
-                <th class="font-bold pb-3 text-left border-b border-primary">
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr  v-for="(sale, index) in shiftSales" :key="sale.id"
-                class="mb-4"
-                :class="{ 'bg-[#EDEDED]': index % 2 == 0 }"
-            >
-                <td class="py-2 pl-3 rounded-l-full">
-                    {{ sale.product.name }}
-                </td>
-                <td class="py-2 pr-2">
-                    {{ sale.quantity }}
-                </td>
-                <td class="py-2 pr-2">
-                    ${{ (sale.price * sale.quantity)?.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}
-                </td>
-                <td class="py-2 pr-2">
-                    <i @click="editSale(sale)" class="fa-solid fa-pencil text-primary text-xs cursor-pointer ml-1"></i>
-                </td>
-            </tr>
-        </tbody>
-    </table>
+    <thead>
+      <tr class="text-center">
+        <th class="font-bold pb-1 pl-2 text-left border-b border-primary">
+          Producto
+        </th>
+        <th class="font-bold pb-1 text-left border-b border-primary">
+          Cantidad
+        </th>
+        <th class="font-bold pb-1 text-left border-b border-primary">
+          Total
+        </th>
+        <th class="font-bold pb-3 text-left border-b border-primary">
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(sale, index) in shiftSales" :key="sale.id" class="mb-4" :class="{ 'bg-[#EDEDED]': index % 2 == 0 }">
+        <td class="py-2 pl-3 rounded-l-full">
+          {{ sale.product.name }}
+        </td>
+        <td class="py-2 pr-2">
+          {{ sale.quantity }}
+        </td>
+        <td class="py-2 pr-2">
+          ${{ (sale.price * sale.quantity)?.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") }}
+        </td>
+        <td class="py-2 pr-2">
+          <i @click="editSale(sale)" class="fa-solid fa-pencil text-primary text-xs cursor-pointer ml-1"></i>
+        </td>
+      </tr>
+    </tbody>
+  </table>
 
-    <DialogModal :show="show_edit_sale_modal" @close="show_edit_sale_modal = false">
-      <template #title>
-        <p class="text-sm font-bold">Editar venta <span class="text-primary font-bold text-sm ml-3">{{ edit_sale.product.name }}</span></p>
-      </template>
-      <template #content>
-        <div class="mb-3 w-full">
-          <InputLabel value="Cantidad *" class="ml-3 mb-1 text-sm" />
-          <input v-model="form.quantity" type="number" autocomplete="off" required class="input"
-           placeholder="00" />
-        </div>
+  <DialogModal :show="show_edit_sale_modal" @close="show_edit_sale_modal = false">
+    <template #title>
+      <p class="text-sm font-bold">Editar venta <span class="text-primary font-bold text-sm ml-3">{{
+        edit_sale.product.name }}</span></p>
+    </template>
+    <template #content>
+      <div class="mb-3 w-full">
+        <InputLabel value="Cantidad *" class="ml-3 mb-1 text-sm" />
+        <input v-model="form.quantity" type="number" autocomplete="off" required class="input" placeholder="00" />
+      </div>
 
-      </template>
-      <template #footer>
-        <CancelButton class="!rounded-full" @click="show_edit_sale_modal = false">Cancelar</CancelButton>
-        <PrimaryButton @click="updateSale" class="ml-2" :disabled="form.processing">Guardar</PrimaryButton>
-      </template>
-    </DialogModal>
+    </template>
+    <template #footer>
+      <CancelButton class="!rounded-full" @click="show_edit_sale_modal = false">Cancelar</CancelButton>
+      <PrimaryButton @click="updateSale" class="ml-2" :disabled="form.processing">Guardar</PrimaryButton>
+    </template>
+  </DialogModal>
 
 </template>
 
@@ -64,45 +61,45 @@ import InputLabel from "@/Components/InputLabel.vue";
 import { useForm } from "@inertiajs/inertia-vue3";
 
 export default {
-data(){
+  data() {
     const form = useForm({
       quantity: null,
     });
 
     return {
-        form,
-        show_edit_sale_modal: false,
-        edit_sale: null,
+      form,
+      show_edit_sale_modal: false,
+      edit_sale: null,
     }
-},
-components:{
-DialogModal,
-CancelButton,
-PrimaryButton,
-InputLabel
-},
-props:{
-shiftSales: Array
-},
-emits:['update-sale'],
-methods:{
+  },
+  components: {
+    DialogModal,
+    CancelButton,
+    PrimaryButton,
+    InputLabel
+  },
+  props: {
+    shiftSales: Array
+  },
+  emits: ['update-sale'],
+  methods: {
     editSale(sale) {
-    this.edit_sale = sale;
-    this.form.quantity = sale.quantity;
-    this.show_edit_sale_modal = true;
-},
+      this.edit_sale = sale;
+      this.form.quantity = sale.quantity;
+      this.show_edit_sale_modal = true;
+    },
     updateSale() {
-        this.form.put(route('sales.update', this.edit_sale.id), {
+      this.form.put(route('sales.update', this.edit_sale.id), {
         onSuccess: () => {
           this.show_edit_sale_modal = false;
           let sale = this.shiftSales.find(item => this.edit_sale.id === item.id);
           if (!sale)
             sale = this.shiftSales.find(item => this.edit_sale.id === item.id);
-          
+
           sale.quantity = this.form.quantity;
         },
       });
     }
-}
+  }
 }
 </script>

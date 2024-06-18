a6c5cd2a48fef2b27d6a35c4489b9c570948a421
Miguel Vázquez <miguelvz26.mv@gmail.com>
2024-06-18 15:36:47 -0600

    mostrar correctamente dia y monto de comisión en nomina de empleado

--- routes/web.php
+++ routes/web.php
@@ -29,15 +29,6 @@ use Inertia\Inertia;
 
 Route::redirect('/', 'login');
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
